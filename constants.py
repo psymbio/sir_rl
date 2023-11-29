fa79a805226d95dbb6d3cefdfb56ad563b779849
psymbio <psymbio@gmail.com>
2023-11-29 11:09:40 +0530

    all code

---
+++ constants.py
@@ -0,0 +1,8 @@
+# Countries to try out: ['JPN' 'AUS' 'DEU' 'ZAF' 'MEX' 
+# 'TUR' 'FRA' 'ITA' 'BRA' 'GBR' 'CAN' 'KOR' 'USA' 'ESP'
+# 'IDN' 'IND' 'CHN' 'A5M' 'LUX']
+# because GDP data is only present for them
+LOCATION_CHOOSEN = "IND"
+OUTPUT_DIR = "output"
+DATA_CACHE_DIR = "data_cache"
+MODELS_DIR = "models"
